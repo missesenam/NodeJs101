8a3cf290038ce29d78a2014f86dbe675fb8a1e10
missesenam <moneyesenam@gmail.com>
2025-02-25 14:46:35 +0100

    my commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const http = require("http");
+const fs = require("fs");
 
 const handleAllRequests = (req, res) => {
-  console.log("just received a request");
+  console.log(`just received a request at ${req.url}`);
+
   const url = req.url;
   if (url === "/") {
     res.write("<h1>Welcome to my Node.js Server</h1>");
@@ -9,6 +11,24 @@ const handleAllRequests = (req, res) => {
     res.write("<h1>This is the About Page</h1>");
   } else if (url === "/contact") {
     res.write("<h1>Contact us at: <a>example@example.com</a>");
+  } else if (url === "/services") {
+    res.write(
+      "<h1>We offer Web Development and Mobile App Development services</h1>"
+    );
+  } else if (url === "/time") {
+    const currentTime = new Date().toLocaleTimeString();
+    res.write(`<h1>The current server time is :${currentTime}</h1>`);
+  } else if (url === "/home") {
+    fs.readFile("index.html", (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end("<h1>Internal Server Error</h1>");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+    return;
   } else {
     res.write("<h1>404 Page Not Found</h1>");
   }
@@ -17,5 +37,5 @@ const handleAllRequests = (req, res) => {
 const server = http.createServer(handleAllRequests);
 
 server.listen(5000, "127.0.0.1", () => {
-  console.log("the server is up and running");
+  console.log("the server is up and running on 5000");
 });
